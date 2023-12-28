710c1a67630f977db1d2afbd9f8f836a8b851826
fajri1997 <f.a.f.s.m.alhusaini@edu.salford.ac.uk>
2023-12-28 13:21:05 +0000

    S

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:meditation_app/main.dart';
 
-//u are gay
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
