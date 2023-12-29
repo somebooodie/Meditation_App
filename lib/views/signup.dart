be9479de0158dc6fbca250c7b7ec19fcaaa51bba
fajri1997 <f.a.f.s.m.alhusaini@edu.salford.ac.uk>
2023-12-29 13:08:22 +0000

    t

---
+++ lib/views/signup.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:meditation_app/models/user.dart';
+import 'package:provider/provider.dart';
+
+class SignupPage extends StatefulWidget {
+  SignupPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final passwordController = TextEditingController();
+
+  final usernameController = TextEditingController();
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sign up"),
+        automaticallyImplyLeading: false,
+      ),
+      resizeToAvoidBottomInset: false,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            const Text("Sign Up"),
+            TextField(
+              decoration: const InputDecoration(hintText: 'Username'),
+              controller: usernameController,
+            ),
+            TextField(
+              decoration: const InputDecoration(hintText: 'Password'),
+              controller: passwordController,
+              obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final User user = User(
+                    username: usernameController.text,
+                    password: passwordController.text);
+                context.read<AuthProvider>().signup(user: user).then((token) {
+                  if (token.isNotEmpty) {
+                    context.pushNamed("signin");
+                  }
+                });
+              },
+              child: const Text("Sign Up"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
